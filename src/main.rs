dc9479a1025a448bc9e852259c4cb6e98021dc39
CodingFlow <3643313+CodingFlow@users.noreply.github.com>
2023-10-18 16:25:14 -0400

    Extract server setup into function.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
-use warp::Filter;
+use std::collections::HashMap;
+
+use warp::{http::Response, Filter};
 
 #[tokio::main]
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let filter = create_filter();
 
+    set_up_server(filter).await;
+}
+
+async fn set_up_server(
+    filter: impl Filter<Extract = impl warp::Reply, Error = warp::Rejection>
+        + Clone
+        + Sync
+        + Send
+        + 'static,
+) {
     warp::serve(filter).run(([127, 0, 0, 1], 3030)).await;
 }
 
 fn create_filter() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    return warp::path!("hello" / String).map(|name| format!("Hello, {}!!", name));
+    warp::get()
+        .and(warp::path!("hello" / String))
+        // .map(|name| format!("Hello, {}!!", name))
+        .and(warp::query::<HashMap<String, String>>())
+        .map(
+            |name: String, p: HashMap<String, String>| match p.get("key") {
+                Some(key) => Response::builder().body(format!("key = {}. Hello {}!!!", key, name)),
+                None => Response::builder().body(String::from("No \"key\" param in query.")),
+            },
+        )
 }
