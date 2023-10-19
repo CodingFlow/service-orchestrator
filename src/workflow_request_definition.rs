77f350758faee9a40fd2148ad7013331ce2ad0b5
CodingFlow <3643313+CodingFlow@users.noreply.github.com>
2023-10-19 19:21:32 -0400

    - Add example open API spec. - Read from open API spec file and use GET method if present.

--- src/workflow_request_definition.rs
+++ src/workflow_request_definition.rs
@@ -1,12 +1,19 @@
 use std::collections::HashMap;
 
+use oas3::Spec;
+use serde::{Deserialize, Serialize};
 use warp::{reject::Rejection, Filter};
 
 const PATH_PARTS: [&str; 2] = ["hello", "bye"];
 
+#[derive(Serialize, Deserialize)]
+struct WorkflowRequestConfig {}
+
 pub fn define_request(
 ) -> impl Filter<Extract = (String, HashMap<String, String>), Error = warp::Rejection> + Clone {
-    let http_method = define_method();
+    let spec = parse_config();
+
+    let http_method = define_method(spec);
 
     let with_paths = define_paths(http_method);
 
@@ -15,8 +22,17 @@ pub fn define_request(
     with_query
 }
 
-fn define_method() -> impl Filter<Extract = (), Error = Rejection> + Copy {
-    warp::get()
+fn define_method(spec: Spec) -> impl Filter<Extract = (), Error = Rejection> + Copy {
+    let path_item = match spec.paths.first_key_value() {
+        Some(item) => item.1,
+        None => panic!("Endpoint method missing"),
+    };
+
+    if path_item.get.is_some() {
+        warp::get()
+    } else {
+        panic!("could not get endpoint method");
+    }
 }
 
 fn define_paths(
@@ -32,4 +48,13 @@ fn define_query(
     with_paths: impl Filter<Extract = (String,), Error = Rejection> + Copy,
 ) -> impl Filter<Extract = (String, HashMap<String, String>), Error = Rejection> + Copy {
     with_paths.and(warp::query::<HashMap<String, String>>())
+}
+
+fn parse_config() -> oas3::Spec {
+    let spec = match oas3::from_path("./src/workflow_request.yaml") {
+        Ok(spec) => spec,
+        Err(_) => panic!("unable to read open API spec file"),
+    };
+
+    spec
 }
