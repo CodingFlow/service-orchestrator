e9733fdec4d6c551edffae728e143099e40f247a
CodingFlow <3643313+CodingFlow@users.noreply.github.com>
2023-10-18 21:38:44 -0400

    Extract workflow request query parameter logic into separate function.

--- src/workflow_request_definition.rs
+++ src/workflow_request_definition.rs
@@ -8,9 +8,11 @@ pub fn define_request(
 ) -> impl Filter<Extract = (String, HashMap<String, String>), Error = warp::Rejection> + Clone {
     let http_method = define_method();
 
-    define_paths(http_method)
-        .and(warp::path::param::<String>())
-        .and(warp::query::<HashMap<String, String>>())
+    let with_paths = define_paths(http_method);
+
+    let with_query = define_query(with_paths);
+
+    with_query
 }
 
 fn define_method() -> impl Filter<Extract = (), Error = Rejection> + Copy {
@@ -19,8 +21,15 @@ fn define_method() -> impl Filter<Extract = (), Error = Rejection> + Copy {
 
 fn define_paths(
     http_method: impl Filter<Extract = (), Error = warp::reject::Rejection> + Copy,
-) -> impl Filter<Extract = (), Error = warp::reject::Rejection> + Copy {
+) -> impl Filter<Extract = (String,), Error = warp::reject::Rejection> + Copy {
     http_method
         .and(warp::path(PATH_PARTS[0]))
+        .and(warp::path::param::<String>())
         .and(warp::path(PATH_PARTS[1]))
+}
+
+fn define_query(
+    with_paths: impl Filter<Extract = (String,), Error = Rejection> + Copy,
+) -> impl Filter<Extract = (String, HashMap<String, String>), Error = Rejection> + Copy {
+    with_paths.and(warp::query::<HashMap<String, String>>())
 }
