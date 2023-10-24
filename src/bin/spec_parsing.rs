90c6c3818b73653fb8dec05a925fc8cf0636d1f4
CodingFlow <3643313+CodingFlow@users.noreply.github.com>
2023-10-24 19:49:20 -0400

    Handle other schema types in generate_code.parse_schema().

--- src/bin/spec_parsing.rs
+++ src/bin/spec_parsing.rs
@@ -2,7 +2,7 @@ use oas3::{spec::SchemaType, Schema, Spec};
 
 #[derive(Clone, Debug)]
 pub struct ParsedSchema {
-    name: String,
+    name: Option<String>,
     schema_type: SchemaType,
     properties: Option<Vec<ParsedSchema>>,
 }
@@ -14,31 +14,42 @@ pub fn parse_schema(
     named_schemas
         .iter()
         .map(|(name, schema)| -> ParsedSchema {
-            let properties: Vec<(Option<String>, Schema)> = schema
-                .properties
-                .iter()
-                .map(
-                    |(name, property_schema_reference)| -> (Option<String>, Schema) {
-                        (
-                            Some(name.to_string()),
-                            property_schema_reference.resolve(&spec).unwrap(),
-                        )
-                    },
-                )
-                .collect();
-
-            let parsed_properties: Vec<ParsedSchema> = parse_schema(properties, &spec)
-                .iter()
-                .map(|parsed_schema| -> ParsedSchema { parsed_schema.clone() })
-                .collect();
-
-            ParsedSchema {
-                name: name.as_deref().unwrap().to_string(),
-                properties: Some(parsed_properties),
-                schema_type: schema.schema_type.unwrap(),
+            match schema.schema_type.unwrap() {
+                SchemaType::Object => parse_object(schema, spec, name),
+                _ => ParsedSchema {
+                    name: name.clone(),
+                    schema_type: schema.schema_type.unwrap(),
+                    properties: None,
+                },
             }
         })
         .collect()
+}
+
+fn parse_object(schema: &Schema, spec: &Spec, name: &Option<String>) -> ParsedSchema {
+    let properties: Vec<(Option<String>, Schema)> = schema
+        .properties
+        .iter()
+        .map(
+            |(name, property_schema_reference)| -> (Option<String>, Schema) {
+                (
+                    Some(name.to_string()),
+                    property_schema_reference.resolve(&spec).unwrap(),
+                )
+            },
+        )
+        .collect();
+
+    let parsed_properties: Vec<ParsedSchema> = parse_schema(properties, &spec)
+        .iter()
+        .map(|parsed_schema| -> ParsedSchema { parsed_schema.clone() })
+        .collect();
+
+    ParsedSchema {
+        name: name.clone(),
+        properties: Some(parsed_properties),
+        schema_type: schema.schema_type.unwrap(),
+    }
 }
 
 fn to_string_schema_type_primitive(schema_type: SchemaType) -> &'static str {
